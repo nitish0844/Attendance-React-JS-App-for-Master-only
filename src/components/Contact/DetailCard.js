b0b7e2f48f05c4500044d9fe4ad1cc17bdcc4db8
Nitish <nitishcooper@gmail.com>
2023-09-12 01:19:41 +0530

    update

--- src/components/Contact/DetailCard.js
+++ src/components/Contact/DetailCard.js
@@ -3,7 +3,7 @@ import Custom from "./custom.png";
 
 const DetailCard = () => {
   return (
-    <div className="flex md:flex-col items-center justify-center w-full bg-Tiger-black h-screen-50 md:pt-0 sm:pt-32 md:space-x-0 sm:space-x-[-30%]">
+    <div className="flex md:flex-col items-center justify-center w-full bg-Tiger-black h-screen-50 md:pt-0 pt-32 md:space-x-0 space-x-[-30%]">
       {/* Photo */}
       <div className="md:w-1/2 w-full p-4 flex justify-center items-center">
         <div className="w-36 h-36 rounded-full bg-TigerColor">
